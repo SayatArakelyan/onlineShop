a81b597e2c385e4cf160cde767f612c058127f99
Sayat Arakelyan <sayat.araqelyan9725@gmail.com>
2023-01-19 10:40:50 +0300

    search product

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // fetch('https://fakestoreapi.com/products')
-//     .then(res=>res.json())
-//     .then(json=> json.map(element => console.log(element.category)))
+//     .then(res => res.json())
+//     .then(json => json.map(element => console.log(element)))
 
 
 const categorySort = document.getElementById("category__sort")
@@ -18,7 +18,7 @@ fetch('https://fakestoreapi.com/products')
     .then(res => res.json())
     .then(json => json.forEach(item => gallery.innerHTML += `
 <div class="gallery__block">
-<figure><img src="${item.image}" class="product">
+<figure><img src="${item.image}" class="product" alt="#">
 </figure>
 <h3 class="product__title">${item.title}</h3>
 <h3 class="price">${item.price}$</h3>
@@ -28,14 +28,37 @@ fetch('https://fakestoreapi.com/products')
 const featured = document.getElementById("featured")
 
 fetch('https://fakestoreapi.com/products')
-    .then(res=>res.json())
-    .then(json => json.splice(17,20))
-    .then(json=> json.forEach(item=> featured.innerHTML+=`<div class="featured__block">
-<figure class="featured__block"><img src="${item.image}" class="featured__img">
+    .then(res => res.json())
+    .then(json => json.splice(17, 20))
+    .then(json => json.forEach(item => featured.innerHTML += `<div class="featured__block">
+<figure class="featured__block"><img src="${item.image}" class="featured__img" alt="#">
 </figure>
 <h3 class="featured__title">${item.price}$</h3>
 
-</div>` ))
+</div>`))
+
+const search = document.querySelector("#search")
+console.log(search)
+
+search.oninput = function () {
+    let val = this.value.toLowerCase().trim()
+
+    let searchItem = document.querySelectorAll(".gallery .gallery__block")
+    if (val !== "") {
+        searchItem.forEach(function (item) {
+            if (item.innerText.search(val) === -1) {
+                item.classList.add("hide")
+            } else {
+                item.classList.remove("hide")
+            }
+        });
+    }
+else {searchItem.forEach(function (item){
+    item.classList.remove("hide")
+    })}
+}
+
+
 
 
 
